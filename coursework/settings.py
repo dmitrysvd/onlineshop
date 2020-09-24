faa3dc099b60de382d99409ddd2c56b8f5f32220
Dmitry Svobodiannikov <svobodiannikov@gmail.com>
2020-09-24 21:57:46 +0300

    Create onlinestore application

--- coursework/settings.py
+++ coursework/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'onlinestore.apps.OnlinestoreConfig',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
