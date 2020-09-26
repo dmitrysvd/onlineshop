fe561e9375a628f1c2f5050b67a04ecfc47a7498
Dmitry Svobodiannikov <svobodiannikov@gmail.com>
2020-09-26 13:33:31 +0300

    Add simple pages: categories, product list, product info

--- onlinestore/urls.py
+++ onlinestore/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('categories/', views.categories, name='categories'),
+    path('products/category/<int:category_id>',
+         views.product_list,
+         name='product_list'),
+    path('product/<int:product_id>', views.product, name='product'),
 ]
--- onlinestore/views.py
+++ onlinestore/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Category, Product
 
 
 def index(request):
-    return render(request, 'onlinestore/index.html')
+    return redirect('categories')
+
+
+def categories(request):
+    categories = Category.objects.all()
+    context = {'categories': categories}
+    return render(request, 'onlinestore/categories.html', context=context)
+
+
+def product_list(request, category_id):
+    products = Product.objects.filter(category__id=category_id)
+    context = {'products': products}
+    return render(request, 'onlinestore/products.html', context=context)
+
+
+def product(request, product_id):
+    product = Product.objects.get(pk=product_id)
+    context = {'product': product}
+    return render(request, 'onlinestore/product.html', context=context)
