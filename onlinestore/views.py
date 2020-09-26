88d3961be45138d803d2ad428b384d8e425c1344
Dmitry Svobodiannikov <svobodiannikov@gmail.com>
2020-09-26 14:28:51 +0300

    Some changes in templates

--- onlinestore/views.py
+++ onlinestore/views.py
@@ -15,10 +15,10 @@ def categories(request):
 def product_list(request, category_id):
     products = Product.objects.filter(category__id=category_id)
     context = {'products': products}
-    return render(request, 'onlinestore/products.html', context=context)
+    return render(request, 'onlinestore/product_list.html', context=context)
 
 
 def product(request, product_id):
     product = Product.objects.get(pk=product_id)
     context = {'product': product}
-    return render(request, 'onlinestore/product.html', context=context)
+    return render(request, 'onlinestore/product_info.html', context=context)
