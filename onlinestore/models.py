aa5cbb223ef8f9755ce78c8b0b88366bb29efce5
Dmitry Svobodiannikov <svobodiannikov@gmail.com>
2020-09-26 12:35:46 +0300

    Add simple models: Product, Category

--- onlinestore/models.py
+++ onlinestore/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='images/products/', blank=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
