9b02d4ad649c3a6d5af78edfa6d9883c5feae2ee
Dmitry Svobodiannikov <svobodiannikov@gmail.com>
2020-09-29 13:26:08 +0300

    Add models: Order, OrderProduct

--- onlinestore/models.py
+++ onlinestore/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 
@@ -10,10 +11,31 @@ class Category(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
-    image = models.ImageField(upload_to='images/products/', blank=True)
+    image = models.ImageField(blank=True,
+                              upload_to='images/products/')
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
     price = models.DecimalField(max_digits=7, decimal_places=2)
     description = models.TextField(blank=True)
+    quantity_in_stock = models.IntegerField(default=0)
+
+    def is_available(self):
+        return self.quantity > 0
 
     def __str__(self):
         return self.name
+
+
+class OrderProduct(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+
+class Order(models.Model):
+    products = models.ManyToManyField(OrderProduct)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    ordered = models.BooleanField(default=False)
+    ordered_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.user.username
