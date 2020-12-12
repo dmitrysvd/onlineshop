19360041331b03bed388f76a27b635e37ed54da2
Dmitry Svobodiannikov <svobodiannikov@gmail.com>
2020-12-13 00:30:42 +0300

    Add category show to the main view

--- onlinestore/models.py
+++ onlinestore/models.py
@@ -6,6 +6,8 @@ from django.urls import reverse
 class Category(models.Model):
     name = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
+    image = models.ImageField(blank=True,
+                              upload_to='images/categories/')
 
     class Meta:
         ordering = ('name',)
