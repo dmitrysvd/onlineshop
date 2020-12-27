41c39d0631986c5666e98dc8f55a749890d4efea
Dmitry Svobodiannikov <svobodiannikov@gmail.com>
2020-12-27 03:16:27 +0300

    Add new tests

--- onlinestore/tests.py
+++ onlinestore/tests.py
@@ -1,16 +1,17 @@
 from django.test import TestCase
 from django.urls import reverse
-from onlinestore.models import Product, Category
+from onlinestore.models import Brand, Product, Category
 
 
-def create_products(count):
+def create_products(count, category=None):
     '''
     Create a given number of products
     '''
-    category = Category(name='Test category',
-                        slug='test-category',
-                        image='/static/onlinestore/images/no-product-image.png')
-    category.save()
+    if not category:
+        category = Category(name='Test category',
+                            slug='test-category',
+                            image='/static/onlinestore/images/no-product-image.png')
+        category.save()
     products = []
     for i in range(count):
         product = Product.objects.create(name=f'Product №{i + 1}',
@@ -101,3 +102,120 @@ class MainViewTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.context['sale_item'],
                          product_with_discount)
+
+
+class ProductListViewTests(TestCase):
+
+    def setUp(self):
+        self.max_products_per_page = 12
+        self.category = Category.objects.create(name='test_category',
+                                                slug='test-category')
+
+    def test_no_products(self):
+        response = self.client.get(self.category.get_absolute_url())
+        self.assertContains(response, 'test_category')
+        self.assertContains(response, 'не найдено')
+
+    def test_products_amount_eq_max(self):
+        products = create_products(
+            count=self.max_products_per_page, category=self.category)
+        response = self.client.get(self.category.get_absolute_url())
+        self.assertEqual(response.status_code, 200)
+        page_obj = response.context['page_obj']
+        self.assertQuerysetEqual(page_obj.object_list,
+                                 [
+                                     '<Product: Product №1>',
+                                     '<Product: Product №2>',
+                                     '<Product: Product №3>',
+                                     '<Product: Product №4>',
+                                     '<Product: Product №5>',
+                                     '<Product: Product №6>',
+                                     '<Product: Product №7>',
+                                     '<Product: Product №8>',
+                                     '<Product: Product №9>',
+                                     '<Product: Product №10>',
+                                     '<Product: Product №11>',
+                                     '<Product: Product №12>',
+                                 ],
+                                 ordered=False)
+        self.assertEqual(page_obj.paginator.num_pages, 1)
+
+    def test_products_amount_more_than_max_per_page(self):
+        products = create_products(count=self.max_products_per_page + 1,
+                                   category=self.category)
+        response = self.client.get(self.category.get_absolute_url())
+        self.assertEqual(response.status_code, 200)
+        page_obj = response.context['page_obj']
+        self.assertQuerysetEqual(page_obj.object_list,
+                                 [
+                                     '<Product: Product №13>',
+                                     '<Product: Product №12>',
+                                     '<Product: Product №11>',
+                                     '<Product: Product №10>',
+                                     '<Product: Product №9>',
+                                     '<Product: Product №8>',
+                                     '<Product: Product №7>',
+                                     '<Product: Product №6>',
+                                     '<Product: Product №5>',
+                                     '<Product: Product №4>',
+                                     '<Product: Product №3>',
+                                     '<Product: Product №2>',
+                                 ],
+                                 ordered=False)
+        self.assertEqual(page_obj.number, 1)
+        self.assertEqual(page_obj.paginator.num_pages, 2)
+
+
+class ProductDetailViewTests(TestCase):
+
+    def setUp(self):
+        self.category = Category.objects.create(name='Test category',
+                                                slug='test-category')
+        self.brand = Brand.objects.create(name='My brand')
+        self.product = Product.objects.create(name='Test product',
+                                              price=102700,
+                                              slug='test-product',
+                                              category=self.category,
+                                              brand=self.brand,
+                                              model_name='Model name',
+                                              engine_power=113,
+                                              engine_type='gas',
+                                              number_of_seats=132,
+                                              year_of_issue=2017)
+
+    def test_try_to_open_not_existing_product(self):
+        response = self.client.get(reverse('onlinestore:product_detail',
+                                           args=[3]))
+        self.assertEqual(response.status_code, 404)
+
+    def test_open_existing_product_without_discount(self):
+        response = self.client.get(reverse('onlinestore:product_detail',
+                                           args=[self.product.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Test product')
+        self.assertContains(response, '102700')
+        self.assertContains(response, self.category.name)
+        self.assertContains(response, 'My brand')
+        self.assertContains(response, 'Model name')
+        self.assertContains(response, '113')
+        self.assertContains(response, 'Бензиновый')
+        self.assertContains(response, '132')
+        self.assertContains(response, '2017')
+
+    def test_open_existing_product_with_discount(self):
+        self.product.discount_price = 99415
+        self.product.sale = True
+        self.product.save()
+        response = self.client.get(reverse('onlinestore:product_detail',
+                                           args=[self.product.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Test product')
+        self.assertContains(response, '102700')
+        self.assertContains(response, '99415')
+        self.assertContains(response, self.category.name)
+        self.assertContains(response, 'My brand')
+        self.assertContains(response, 'Model name')
+        self.assertContains(response, '113')
+        self.assertContains(response, 'Бензиновый')
+        self.assertContains(response, '132')
+        self.assertContains(response, '2017')
