096ec3d1055a9293d6fd3ba389764a191b1619b0
Dmitry Svobodiannikov <svobodiannikov@gmail.com>
2021-01-06 04:08:21 +0300

    Rewrite templates using Bootstrap 5

--- onlinestore/templatetags/onlinestore_tags.py
+++ onlinestore/templatetags/onlinestore_tags.py
@@ -1,10 +1,8 @@
 from django import template
-from ..models import Product
 
 register = template.Library()
 
 
-@register.inclusion_tag('onlinestore/product_slider.html')
-def product_slider(products, title=None):
-    return {'product_slider_list': products,
-            'title': title}
+@register.inclusion_tag('onlinestore/product_item.html')
+def product_item(product):
+    return {'product': product}
