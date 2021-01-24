624977b64b2f29b3b6c1a5edeae232b31989468f
Dmitry Svobodiannikov <svobodiannikov@gmail.com>
2021-01-24 17:01:58 +0300

    Add order_detail view to the administration site

--- orders/models.py
+++ orders/models.py
@@ -26,6 +26,9 @@ class Order(models.Model):
     def get_total_cost(self):
         return sum(item.get_cost() for item in self.items.all())
 
+    def get_total_quantity(self):
+        return sum(item.quantity for item in self.items.all())
+
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order,
--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
-from .models import OrderItem
+from django.shortcuts import get_object_or_404, render, redirect
+from django.contrib.admin.views.decorators import staff_member_required
+from .models import OrderItem, Order
 from .forms import OrderCreateForm
 from .tasks import send_email_about_order
 from cart.cart import Cart
@@ -31,3 +32,11 @@ def create_order(request):
 def order_created(request):
     return render(request,
                   'orders/order_created.html')
+
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request,
+                  'admin/orders/order_detail.html',
+                  {'order': order})
